9900e19b8d50d3b17f6bdeb12fb4ef84e9b5d13a
cquangninh <cquangninh@gmail.com>
2022-11-14 21:52:06 +0700

    1 file modified, 1 added

--- angular-14-client/src/app/services/tutorial.service.ts
+++ angular-14-client/src/app/services/tutorial.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
@@ -10,33 +10,52 @@ const baseUrl =
   providedIn: 'root',
 })
 export class TutorialService {
+  headers = new HttpHeaders().set(
+    'access-control-allow-origin',
+    'https://graphql-ybekqz--8080.local-credentialless.webcontainer.io/'
+  );
+
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(baseUrl);
+    return this.http.get<Tutorial[]>(baseUrl, {
+      headers: this.headers,
+    });
   }
 
   get(id: any): Observable<Tutorial> {
-    return this.http.get<Tutorial>(`${baseUrl}/${id}`);
+    return this.http.get<Tutorial>(`${baseUrl}/${id}`, {
+      headers: this.headers,
+    });
   }
 
   create(data: any): Observable<any> {
-    return this.http.post(baseUrl, data);
+    return this.http.post(baseUrl, data, {
+      headers: this.headers,
+    });
   }
 
   update(id: any, data: any): Observable<any> {
-    return this.http.put(`${baseUrl}/${id}`, data);
+    return this.http.put(`${baseUrl}/${id}`, data, {
+      headers: this.headers,
+    });
   }
 
   delete(id: any): Observable<any> {
-    return this.http.delete(`${baseUrl}/${id}`);
+    return this.http.delete(`${baseUrl}/${id}`, {
+      headers: this.headers,
+    });
   }
 
   deleteAll(): Observable<any> {
-    return this.http.delete(baseUrl);
+    return this.http.delete(baseUrl, {
+      headers: this.headers,
+    });
   }
 
   findByTitle(title: any): Observable<Tutorial[]> {
-    return this.http.get<Tutorial[]>(`${baseUrl}?title=${title}`);
+    return this.http.get<Tutorial[]>(`${baseUrl}?title=${title}`, {
+      headers: this.headers,
+    });
   }
 }
